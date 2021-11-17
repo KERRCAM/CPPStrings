0d4fe6bca66f3e21da42d937eee64874b0a44d2a
KERRCAM <kerrcameron@hotmail.com>
2021-11-17 22:34:38 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C__Strings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Strings.dir/main.cpp.o"
+  "C__Strings"
+  "C__Strings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Strings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
